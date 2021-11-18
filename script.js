42d2ddfaf69e8e8e26e1ff2a56b9a8b0b7ed3a7f
rachel-meadows <rachelnewzealand@gmail.com>
2021-11-18 22:39:19 +1300

    Add logo and nav bar

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+PLAN
+Objects:
+    gameboard (module)
+        array: { [ [1, 2, 3], [4, 5, 6], [7, 8 , 9] ] } (or maybe use coordinates)
+        function to render array
+    userPlayer + computerPlayer (make with factory)
+        name
+        score
+    gameControl (module)
+        tokenChoice (submodule)
+        difficultyChoice (submodule)
+        restart (submodule)
+        Whose turn is it?
+        Is this a legal move?
+        Does this trigger an end state? Who wins?
+*/
+
